a700c7c929415c6cd0341d35b17b2ad68fefd450
Shubham <shubham.srathore07@gmail.com>
2022-06-28 22:36:56 +0530

    time-zone

---
+++ js/time.js
@@ -0,0 +1,6 @@
+setInterval(() => {
+    let a = new Date();
+    let date = a.toLocaleDateString();
+    let time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+     document.getElementById('time').innerHTML = date + "on" + time;
+ }, 1000);
